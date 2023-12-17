9385b7632182ad32b1f06ee37daf0a1b3fc7ca39
SadiyAnwari <122463262+SadiyAnwari@users.noreply.github.com>
2023-12-17 09:57:21 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the limit");
+		int n=scan.nextInt();
+		for (i=0; i<=n;i++) {
+			
+		}
+		
+				
+
+	}
+
+}
